382ad82f23a5fe04727996e6c1e60ecc07d8b28f
Marcus Gainey <marcusgainey@Marcuss-MacBook-Air.local>
2022-06-28 16:40:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT || 3001;
+
+//handle CORS error
+app.use(cors());
+
+//mount API router
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(JSON.stringify(req.body, 0, 2));
+  res.status(200).send(req.body);
+});
+
