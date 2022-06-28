5bfa92df1d8cac84b80a086e4cda2e1535d64f07
Marcus Gainey <marcusgainey@Marcuss-MacBook-Air.local>
2022-06-28 17:10:28 -0400

    updates to inital test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const challenge = req.body.challenge;
-  console.log(req.body);
+  console.log(challenge);
   console.log(JSON.stringify({challenge: challenge}));
   res.status(200).send(JSON.stringify({challenge: challenge}));
 });
